684d74c694060d9e3092e4b788f3e0c90f111371
Marley Schneider <marleysch18@gmail.com>
2024-05-14 14:14:16 -0700

    Deleted comment

--- bigintTest.cpp
+++ bigintTest.cpp
@@ -1,10 +1,3 @@
-//Marley Schneider
-//CECS 325-02
-//Prog 6 - BigInt
-//
-//5/9/2024
-//
-//I certify that this program is my own original work. I did not copy any part of this program from any other source. I further certify that I typed each and every line of code in this program
 
 #include <iostream>
 #include <vector>
